6fbb1e2a84e38c8d1d575beb9069adafd36a0de8
Nekkka <nurievaaa28@gmail.com>
2025-04-19 00:39:26 +0500

    .

---
+++ resume_analyzer/resume_analyzer/celery.py
@@ -0,0 +1,15 @@
+# celery.py
+
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resume_analyzer.settings')
+
+app = Celery('resume_analyzer')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+
+app.autodiscover_tasks()
--- resume_analyzer/resume_analyzer/settings.py
+++ resume_analyzer/resume_analyzer/settings.py
@@ -159,3 +159,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # пока без SMTP
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'  
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0' 
+CELERY_TIMEZONE = 'UTC'
